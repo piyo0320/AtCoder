c1c6a8c40da4e0334aedfa2173ae1b126b345222
piyo8810 <piyo8810.8@gmail.com>
2021-11-20 19:06:27 +0900

    練習

---
+++ BeginnersSelection/ABC081A - Placing Marbles/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Count(r => r == '1');
+
+        Console.WriteLine(input);
+    }
+}
+
---
+++ BeginnersSelection/ABC081B - Shift only/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        var input2 = Console.ReadLine().Split(' ').Select(r => uint.Parse(r));
+        var inputMax = input2.Max();
+
+        uint t = 0;
+
+        foreach(var item in input2)
+        {
+            t |= item;
+        }
+
+        var s = 0;
+
+        if (t == 0)
+        {
+            Console.WriteLine(s);
+            return;
+        }
+
+        while ((t & 1) != 1)
+        {
+            s++;
+            t >>= 1;
+        }
+
+        Console.WriteLine(s);
+    }
+}
+
